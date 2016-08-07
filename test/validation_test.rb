253c16f78e13111933658b97f71a34237dd5b08a
Chase Dunagan <cdunagan@utexas.edu>
2016-08-07 16:57:48 -0600

    cleaned up validation test

--- test/validation_test.rb
+++ test/validation_test.rb
@@ -7,26 +7,6 @@ require './lib/grid'
 
 class ValidationTests < Minitest::Test
   include Validation
-  #PRE-BS-WORK
-  # def test_it_is_horizontal_with_no_spaces_for_2_unit_ship
-  #   validator = Validation.new
-  #   assert_equal true, validator.two_unit_ship_positions?(24, 26)
-  #   assert_equal false, validator.two_unit_ship_positions?(24, 28)
-  # end
-  #
-  # def test_it_is_vertical_with_no_spaces_for_2_unit_ship
-  #   validator = Validation.new
-  #   assert_equal true, validator.two_unit_ship_positions?(24, 34)
-  #   assert_equal false, validator.two_unit_ship_positions?(34, 54)
-  #   assert_equal true, validator.two_unit_ship_positions?(50, 40)
-  # end
-  #
-  # def test_if_it_validates_3_unit_ship
-  #   validator = Validation.new
-  #   assert_equal true, validator.three_unit_ship_posistions?(24, 34, 44)
-  #   assert_equal false, validator.three_unit_ship_posistions?(24, 26, 36)
-  #   assert_equal true, validator.three_unit_ship_posistions?(24, 26, 28)
-  # end
 
   def test_if_chooses_a_second_spot_that_is_valid
     grid = Grid.new
