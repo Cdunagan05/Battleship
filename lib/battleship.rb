f5f0aabcd42755d930a5d47d0f9ccfd93ce49463
Ryan Workman <rdavid1099@gmail.com>
2016-08-09 09:25:23 -0600

    Added comments and refactored a bit

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,6 +1,8 @@
 require './lib/messages'
 require './lib/play'
 
+# I'm digging this.  Real succinct and straight to the point.  Great runner file.
+
 puts Messages.intro
 response = gets.chomp
 if response == "i" || response == "instructions"
